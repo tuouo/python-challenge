4e34d5464632931161c47b1bdfdfeb03c5d2e8ea
tuouo <ltytuotuo@gmail.com>
2015-11-11 11:02:58 +0800

    map(*, *)

---
+++ 0005pronounceIt.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+http://www.pythonchallenge.com/pc/def/peak.html
+
+pronounce it
+
+'''
+import urllib.request as ur, pickle, os
+from tools import writeData
+url = 'http://www.pythonchallenge.com/pc/def/banner.p'
+data = ur.urlopen(url).read()
+lists = pickle.loads(data)
+with open(os.path.join(os.getcwd(), "output.rtf"), 'w') as r:
+    for i in lists:
+	    r.write((''.join(map(lambda p: p[0]*p[1], i))) + '\n')
