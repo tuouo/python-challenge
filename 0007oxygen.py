2a672b36dbd3e48f96a91f6a68de4c930b93ddce
tuouo <ltytuotuo@gmail.com>
2015-11-12 10:25:01 +0800

    getpixel((x,y)) map

---
+++ 0007oxygen.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+http://www.pythonchallenge.com/pc/def/oxygen.html
+'''	
+from PIL import Image
+im = Image.open("0007oxygen.png")
+#print(im.format, im.size, im.mode, im.info)
+#print(im.getpixel((0, 0)))
+
+w, h = im.size
+#for i in range(h):
+    #for j in range(13):
+        #r, g, b, a = im.getpixel((j, i))
+        #if r == g and r == b:
+           #print('%s\t%s\t%s' % (i , j ,r))
+
+info = [im.getpixel((i, h // 2))[0] for i in range(0, w, 7)]	# 47 -- 95
+print(info)
+info = ''.join(map(chr, info))
+print(info)
+
+import re
+end = re.findall("\d+", info)
+print(end)
+end = ''.join(map(chr, map(int, end)))
+print(end)
