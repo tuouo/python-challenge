7d6ae41839501af01fb67ebd98bc194ac00e1a74
tuouo <ltytuotuo@gmail.com>
2015-11-13 23:27:54 +0800

    re r'((?P<w>\d)(?p=W)*)'

--- ToBeLegendary.py
+++ ToBeLegendary.py
@@ -6,4 +6,9 @@ To be legendary
 
 2: find the latters only appear once in string.
    then twice. (not record times for every latter)
+
+10: reg = re.compile(r'(?P<w>\d)(?P=w)')
+    reg = re.compile(r'((?P<w>\d)(?P=w)*)')
+    make somebady hnow why
+
 '''
