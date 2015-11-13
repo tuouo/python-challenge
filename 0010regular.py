053f3be3d3dd3fcba39425f006dac5ec56b4917c
tuouo <ltytuotuo@gmail.com>
2015-11-13 23:12:46 +0800

    diff: r'(?P<w>\d)(?P=w)*'

---
+++ 0010regular.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+Difference
+'''
+
+import re
+reg = re.compile(r'(?P<w>\d)(?P=w)*')
+#reg = re.compile(r'((?P<w>\d)(?P=w)*)')
+n = '111221'
+print(reg.findall(n))
+n = '11112221'
+print(reg.findall(n))
+n = '12'
+print(reg.findall(n))
