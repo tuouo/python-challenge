245a8eb5fb0151d6e110aff21974f41d063d9111
tuouo <ltytuotuo@gmail.com>
2015-11-14 16:38:34 +0800

    datetime weekday()

---
+++ 0015whom.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+http://www.pythonchallenge.com/pc/return/uzi.html;charset=utf-8
+'''
+from datetime import date
+#for i in range(1016, 2016, 20):
+    #d = date(i, 1, 27)
+    #if d.weekday() == 1:    #Tuesday
+        #print(d)
+
+print([i for i in range(1016, 2016, 20) if date(i, 1, 27).weekday() == 1])
