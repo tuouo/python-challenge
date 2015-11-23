944aa01b6f3101505b7166ea5322fd483b1edfbc
tuouo <ltytuotuo@gmail.com>
2015-11-23 22:21:17 +0800

    RGB, byte chr

---
+++ 0028ringringring.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+http://www.pythonchallenge.com/pc/ring/bell.html
+'''
+from PIL import Image
+import struct
+im = Image.open("0028bell.png")
+#print(im)
+r, g, b = im.split()
+#r.show()
+#g.show()    #ring ring ring
+#b.show()
+data = list(g.getdata())
+#print(data)
+
+outstand = [abs(data[i] - data[i + 1]) for i in range(0, len(data), 2) if abs(data[i] - data[i + 1]) != 42]
+# print(outstand)
+s = []
+for i in outstand:
+    s.append(chr(i))
+#print(''.join(s))
+import tools
+tools.writeData(''.join(s))
