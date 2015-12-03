62d172f7464cf9facfe9d21595af940c056d1a16
tuouo <ltytuotuo@gmail.com>
2015-12-03 17:51:27 +0800

    only get input data

--- 0032etch.py
+++ 0032etch.py
@@ -4,15 +4,17 @@
 http://www.pythonchallenge.com/pc/rock/arecibo.html
 '''
 import urllib.request as ur, re
-url = "http://www.pythonchallenge.com/pc/rock/warmup.txt"
+url = "http://www.pythonchallenge.com/pc/rock/up.txt"
+# url = "http://www.pythonchallenge.com/pc/rock/warmup.txt"
 mgr = ur.HTTPPasswordMgrWithDefaultRealm()
 mgr.add_password(None, url, 'kohsamui', 'thailand')
 opener = ur.build_opener(ur.HTTPBasicAuthHandler(mgr))
 req = opener.open(url)
 data = req.read()
 #print(data)
-# with open("output.rtf", 'wb') as r:
-#         r.write(data)
+with open("0032up.rtf", 'wb') as r:
+# # with open("0032warmup.rtf", 'wb') as r:
+    r.write(data)
 
 numbers = data.split(b'\n')
 #print(numbers)
@@ -21,4 +23,4 @@ reg = re.compile(b'(\d+)')
 for i in numbers:
     if reg.match(i):
         lines.append(list(reg.findall(i)))
-# print(lines)
+print(lines)
