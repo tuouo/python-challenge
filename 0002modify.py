106421b674f3a73154d35c512823a11e66d48d1e
tuouo <ltytuotuo@gmail.com>
2015-12-21 21:52:10 +0800

    combine

--- 0002modify.py
+++ 0002modify.py
@@ -16,24 +16,3 @@ print(result)
 for i in alph:
     if result[i] == 1:
         print(i, end = '')
-
-# import os
-# result = {}
-# alph = ''
-# alph2 = []
-# with open (os.path.join(os.getcwd(), '0002ocrModify.html'), 'r') as html:
-#     for line in html:
-#         for letter in range(len(line)):
-#             if line[letter] in result:
-#                     result[line[letter]] += 1
-#             else:
-#                 result[line[letter]] = 1
-#                 alph += line[letter]
-#                 alph2.append(line[letter])
-# print(result)
-# for i in alph:
-#     if result[i] == 1:
-#         print(i, end = '')
-# print()
-# print(alph)
-# print(''.join(alph2))
