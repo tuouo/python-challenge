84911c2d3a6a5054597c7fd1df50461893c470f4
tuouo <ltytuotuo@gmail.com>
2015-12-21 21:58:20 +0800

    itertools groupby

--- 0010bull.py
+++ 0010bull.py
@@ -12,3 +12,10 @@ for i in range(30):
     n = "".join(list(map(lambda x: '%s%s' % (len(x[0]), x[1]), reg.findall(n))))
 
 print(len(n))
+
+# from itertools import groupby
+# a = '1'
+# for i in range(5):
+#     a = ''.join(str(len(list(v))) + k for k, v in groupby(a))
+#     print(a)
+# print(len(a))
