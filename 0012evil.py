9488a06cee83154f91573d99192230418d3bc716
tuouo <ltytuotuo@gmail.com>
2015-12-22 22:15:31 +0800

    add web

--- 0012evil.py
+++ 0012evil.py
@@ -9,6 +9,15 @@ http://www.pythonchallenge.com/pc/return/evil.html
 
 with open("0012evil2.gfx", 'rb') as source:
     content = source.read()
+
+# import urllib.request as ur
+# url = "http://www.pythonchallenge.com/pc/return/evil2.gfx"
+# mgr = ur.HTTPPasswordMgrWithDefaultRealm()
+# mgr.add_password(None, url, 'huge', 'file')
+# opener = ur.build_opener(ur.HTTPBasicAuthHandler(mgr))
+# content = opener.open(url).read()
+
+
 for i in range(5):
     with open("0012_%d.jpg" %i, "wb") as new:
         new.write(content[i::5])
