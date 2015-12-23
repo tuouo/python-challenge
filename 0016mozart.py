4e9a46ea4e349d279c300399fde942d34103c943
tuouo <ltytuotuo@gmail.com>
2015-12-23 14:55:50 +0800

    why 195,  cause it appera together and only once

--- 0016mozart.py
+++ 0016mozart.py
@@ -7,19 +7,33 @@ from PIL import Image, ImageChops
 
 im = Image.open('0016mozart.gif')
 w, h = im.size
-#color = {}
-#for i in range(w):
-    #a = im.getpixel((i, 0))
-    #if a in color:
-        #color[a] += 1
-    #else:
-        #color[a] = 1
-#for k, v in color.items():
-    #if v < 10 and v > 2:
-        #print('%s:%s' % (k, v))
-#print(color)
+color = {}
+pre, con = im.getpixel((0, 0)), 0
+color[pre] = [0, 1]    # deal with i == 0
+for i in range(w):
+    a = im.getpixel((i, 0))
+    if a == pre:        
+        color[a][0] += 1
+        con += 1
+        continue
+    if a not in color:
+        color[a] = [1, 1]
+    if color[pre][1] < con:
+        color[pre][1] = con
+    con, pre = 1, a
 
-magic = 195    # do not ask me how did i know 195 means the color(pink?)
+linePoint = 0
+print(color, '\n')
+for k, v in color.items():
+    # # if v[0] < 10 and v[0] > 2:
+    if v[0] > 2:
+        print('%s:%s' % (k, v))
+        if v[0] == v[1]:
+            print("together:", k)
+            linePoint = k
+
+# magic = 195    # do not ask me how did i know 195 means the color(pink?)
+magic = linePoint    #which color appear together
 for j in range(h):
     box = 0, j, w,  j + 1
     row = im.crop(box)
@@ -30,3 +44,5 @@ for j in range(h):
     row = ImageChops.offset(row, -i)
     im.paste(row, box)
 im.show()
+
+
